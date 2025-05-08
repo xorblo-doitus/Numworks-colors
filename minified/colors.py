c47e75821914567138d1f3abebfe8befce989ab0
xorblo-doitus <75997617+xorblo-doitus@users.noreply.github.com>
2025-05-08 11:08:49 +0200

    Import as-is from my calculator

--- minified/colors.py
+++ minified/colors.py
@@ -1,2 +1,19 @@
-'I am template'
-def foo():0
+from kandinsky import*
+from random import*
+wormcolor=color(155,0,155)
+black=color(0,0,0)
+background=color(255,200,140)
+oob=color(255,180,0)
+red=color(248,0,0)
+green=color(0,255,0)
+yellow=color(255,255,0)
+purple=color(0,255,255)
+white=color(255,255,255)
+grey=color(122,122,122)
+white_blue=color(200,255,255)
+blue=color(39,230,228)
+dark_blue=color(0,153,152)
+light_brown=color(255,132,61)
+dark_brown=color(89,37,6)
+gamecolors=black,red,green,yellow,purple,grey
+def random_color():return choice(gamecolors)
--- src/colors.py
+++ src/colors.py
@@ -1,7 +1,25 @@
-"""I am template"""
+from kandinsky import *
+from random import *
 
-def foo() -> None:
-	pass
-	pass
+wormcolor = color(155, 0, 155)
+black = color(0, 0, 0)
+background = color(255, 200, 140)
+oob = color(255, 180, 0)
+red = color(248, 0, 0)
+green = color(0, 255, 0)
+yellow = color(255, 255, 0)
+purple = color(0, 255, 255)
+white = color(255, 255, 255)
+grey = color(122, 122, 122)
 
-# don't forget to rename all the colors files and strings.
+# UI
+white_blue = color(200, 255, 255)
+blue = color(39, 230, 228)
+dark_blue = color(0, 153, 152)
+light_brown = color(255, 132, 61)
+dark_brown = color(89, 37, 6)
+
+gamecolors = (black, red, green, yellow, purple, grey)
+
+def random_color():
+  return choice(gamecolors)
